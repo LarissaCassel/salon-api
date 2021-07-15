8630c354274d52af6b0b6baf32ad7c73b11d847a
Larissa Cassel <lari.cassel@gmail.com>
2021-07-14 22:27:49 -0300

    salon api

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv/config'); 
+const {PORT} = process.env;
+require('./src/services/database');
+
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(morgan('dev')); 
+app.use(cors());
+
+//routes
+app.use('/salon', require('./src/routes/salon.routes')); 
+app.use('/services', require('./src/routes/services.routes'));
+app.use('/collaborator', require('./src/routes/collaborator.routes'));
+app.use('/timeline', require('./src/routes/timeline.routes'));
+app.use('/schedule', require('./src/routes/schedule.routes'));
+
+app.set('port', PORT);
+app.listen(app.get('port'), () => {
+    console.log(`API RODANDO ${app.get('port')}`);
+});
