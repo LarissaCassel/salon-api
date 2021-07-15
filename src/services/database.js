f1c773d2c00dfb609aace406f7595769cd5c6194
Larissa Cassel <lari.cassel@gmail.com>
2021-07-14 22:26:08 -0300

    database config

---
+++ src/services/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {DATABASE_URI} = process.env;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(DATABASE_URI)
+.then( () => console.log('DATABASE IS UP!') )
+.catch( (err) => console.log(`ERRO DATABASE: ${err}`) );
